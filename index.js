9b8d71c327586707164151d1d32c0e29fe6b7b8b
Emmaponysquad <81795340+Emmaponysquad@users.noreply.github.com>
2023-07-10 13:44:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function() {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+  
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  });
